10a35d1f55b75700a4108b0228acea3154d28881
Niedson Araujo da Costa <niedson.araujo.c@gmail.com>
2016-01-17 23:40:20 -0200

    bkp

---
+++ src/com/doeasy/model/daoimpl/PersonDaoImpl.java
@@ -0,0 +1,26 @@
+package com.doeasy.model.daoimpl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.doeasy.model.vo.Person;
+
+@Transactional
+public class PersonDaoImpl {
+ 
+    @PersistenceContext
+    private EntityManager em;
+     
+    public Long save(Person person) {
+        em.persist(person);
+        return person.getId();
+    }
+     
+    public List<Person>getAll() {
+        return em.createQuery("SELECT p FROM Person p", Person.class).getResultList();
+    }
+}
