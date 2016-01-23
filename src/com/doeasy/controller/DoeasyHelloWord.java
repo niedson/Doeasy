6effb9c88c775106a1f1feb7a8a5a1c8da5f6651
Niedson Araujo da Costa <niedson.araujo.c@gmail.com>
2016-01-23 14:32:42 -0200

    conflito

--- src/com/doeasy/controller/DoeasyHelloWord.java
+++ src/com/doeasy/controller/DoeasyHelloWord.java
@@ -1,14 +1,9 @@
 package com.doeasy.controller;
 
-import java.util.List;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.doeasy.model.dao.PersonDao;
-import com.doeasy.model.vo.Person;
 
 @Controller
 public class DoeasyHelloWord {
--- src/com/doeasy/model/dao/PersonDao.java
+++ src/com/doeasy/model/dao/PersonDao.java
@@ -1,13 +1,13 @@
 package com.doeasy.model.dao;
 import java.util.List; 
  
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.doeasy.model.vo.Person;
+//import com.doeasy.model.vo.Person;
  
-@Repository
-public interface PersonDao extends CrudRepository<Person, Long> {
-     
-    public List<Person> findByName(String name);
-}
+//@Repository
+////public interface PersonDao extends CrudRepository<Person, Long> {
+////     
+////    public List<Person> findByName(String name);
+////}
