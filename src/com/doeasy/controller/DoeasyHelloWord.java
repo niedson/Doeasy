aba6bfd2900e3f0586ab201dcfea3c1d3c728fe7
Niedson Araujo da Costa <niedson.araujo.c@gmail.com>
2016-01-24 13:53:47 -0200

    conclusao de testes com hsaldb e alteracao do pom.xml

--- src/com/doeasy/controller/DoeasyHelloWord.java
+++ src/com/doeasy/controller/DoeasyHelloWord.java
@@ -1,18 +1,18 @@
 package com.doeasy.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//
-//@Controller
-//public class DoeasyHelloWord {
-//	
-//	@RequestMapping("/welcome")
-//	public ModelAndView helloWord() {
-//		String message = "<br><div style='text-align:center;'>" 
-//				+ "<h3>********** Welcome Page</h3></div><br><br>";
-//		return new ModelAndView("welcome", "message", message);
-//	}	
-//
-//}
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class DoeasyHelloWord {
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWord() {
+		String message = "<br><div style='text-align:center;'>" 
+				+ "<h3>********** Welcome Page</h3></div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}	
+
+}
